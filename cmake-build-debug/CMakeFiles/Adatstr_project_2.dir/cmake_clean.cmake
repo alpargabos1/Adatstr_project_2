f5d6c898880e2ea643e5e6359545dbefb43bee0c
alpargabos1 <gabosalpar0304@gmail.com>
2021-04-23 21:18:16 +0300

    Init commit

---
+++ cmake-build-debug/CMakeFiles/Adatstr_project_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Adatstr_project_2.exe"
+  "Adatstr_project_2.exe.manifest"
+  "Adatstr_project_2.pdb"
+  "CMakeFiles/Adatstr_project_2.dir/main.c.obj"
+  "libAdatstr_project_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Adatstr_project_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
